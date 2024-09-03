118be6a605b29419ff3e1fb7166bc505309f72e4
Anushka Verma <anushkaverma779@gmail.com>
2024-09-03 15:31:50 +0530

    made some changes for deployment

--- server/server.js
+++ server/server.js
@@ -4,11 +4,18 @@ const db = require("./db");
 require("dotenv").config();
 const passport = require("passport");
 const path = require("path");
+const cors = require('cors');
 
 app.set("view engine", "ejs");
 
 const PORT = process.env.PORT;
 
+const authRoutes = require("./routes/auth");
+const productRoutes = require("./routes/product");
+
+app.use("/auth", authRoutes);
+app.use("/products", productRoutes);
+app.use(cors());
 app.get("/", (req, res) => {
   // res.send("hi welcome to this e-commerce app");
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
@@ -21,12 +28,6 @@ const bodyParser = require("body-parser");
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.use(bodyParser.json()); // we can access it in (req.body)
-
-const authRoutes = require("./routes/auth");
-const productRoutes = require("./routes/product");
-
-app.use("/auth", authRoutes);
-app.use("/products", productRoutes);
 
 app.use(passport.initialize());
 
