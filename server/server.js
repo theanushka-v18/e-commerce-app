7f4055df70d9203aaaa97a595487b63906865c29
Anushka Verma <anushkaverma779@gmail.com>
2024-09-03 15:43:35 +0530

    modified route

--- server/server.js
+++ server/server.js
@@ -6,6 +6,12 @@ const passport = require("passport");
 const path = require("path");
 const cors = require('cors');
 
+app.use(cors());
+// kind of middleware which parse the data
+const bodyParser = require("body-parser");
+app.use(bodyParser.json()); // we can access it in (req.body)
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
 app.set("view engine", "ejs");
 
 const PORT = process.env.PORT;
@@ -13,21 +19,19 @@ const PORT = process.env.PORT;
 const authRoutes = require("./routes/auth");
 const productRoutes = require("./routes/product");
 
-app.use("/auth", authRoutes);
-app.use("/products", productRoutes);
-app.use(cors());
-app.get("/", (req, res) => {
+app.use("https://e-commerce-app-nine-rho.vercel.app/auth", authRoutes);
+app.use("https://e-commerce-app-nine-rho.vercel.app/products", productRoutes);
+app.get("https://e-commerce-app-nine-rho.vercel.app/", (req, res) => {
   // res.send("hi welcome to this e-commerce app");
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
-// kind of middleware which parse the data
-const bodyParser = require("body-parser");
+
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.use(bodyParser.json()); // we can access it in (req.body)
+
 
 app.use(passport.initialize());
 
